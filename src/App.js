472f027668c3d3ae8ac736c6aebf7d1064c0ac2b
Sasa Kocic <sasa.kocic@gmail.com>
2023-05-24 20:40:32 +0200

    snapshot

--- src/App.js
+++ src/App.js
@@ -1,5 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
+import { MyButton } from './MyButton';
+import { Greeting } from './Greeting';
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+const shoppingProducts = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+const products = [
+  { title: 'Cabbage', id: 1 },
+  { title: 'Garlic', id: 2 },
+  { title: 'Apple', id: 3 },
+];
+
+const listItems = products.map(product => 
+  <li key={product.id}>
+    {product.title}
+  </li>
+)
+
+function ListProducts() {
+  return (
+    <ul>{listItems}</ul>
+  )
+}
+
+let showHide = false
+
+export function toggleShowHide() {
+  showHide = true
+  document.querySelector('#show-hide').innerHTML = "Show/hide is " + showHide.toString()
+  alert(showHide.toString())
+}
+
+function ShoppingList() {
+  const shoppingItems = shoppingProducts.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
+  return (
+    <ul>{shoppingItems}</ul>
+  );
+}
+
+function ProfileButton() {
+  return (
+    <button id="show-hide" onClick={toggleShowHide}>Show/hide is {showHide.toString()}</button>
+    //       dangerouslySetInnerHTML={{ __html: "Show/hide is " + showHide.toString() }} />
+  );
+}
+
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
 
 function App() {
   return (
@@ -17,6 +99,15 @@ function App() {
         >
           Learn React
         </a>
+        <h2>Counters that update separately</h2>
+        <MyButton />
+        <MyButton />
+        <ProfileButton />
+        <img className='avatar' src="/avatar.jpeg"/>
+        <Greeting name="world" />
+        {showHide && <Profile />}
+        <ListProducts />
+        <ShoppingList />
       </header>
     </div>
   );
---
+++ src/Greeting.js
@@ -0,0 +1,5 @@
+export function Greeting({ name }) {
+  return (
+    <h2>Hello, {name}</h2>
+  );
+}
