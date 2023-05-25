7f4d55ec57682a99f5a7547167dbe0323754ca0d
Sasa Kocic <sasa.kocic@gmail.com>
2023-05-25 19:15:33 +0200

    used Card and changed title to red

--- src/Profile.js
+++ src/Profile.js
@@ -1,20 +1,27 @@
+import { Card } from 'react-bootstrap';
+import './Profile.css';
+
 export function Profile() {
   const user = {
     name: 'Hedy Lamarr',
     imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
     imageSize: 90,
   };
+
   return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }} />
-    </>
+    <Card>
+      <Card.Body>
+        <Card.Title className="title">{user.name}</Card.Title>
+        <Card.Img
+          className="avatar"
+          src={user.imageUrl}
+          alt={'Photo of ' + user.name}
+          style={{
+            width: user.imageSize,
+            height: user.imageSize
+          }}
+        />
+      </Card.Body>
+    </Card>
   );
 }
