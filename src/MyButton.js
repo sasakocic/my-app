13af8fe12f534cbeee3a7f20be9eeff36ebb0702
Sasa Kocic <sasa.kocic@gmail.com>
2023-05-25 15:59:34 +0200

    Counters that update together

--- src/MyButton.js
+++ src/MyButton.js
@@ -1,14 +1,7 @@
+export function MyButton({count, onClick}) {
 
-import { useState } from 'react';
-
-export function MyButton() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
     return (
-    <button onClick={handleClick}>
+    <button onClick={onClick}>
       Clicked {count} times
     </button>
   );
