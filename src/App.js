3f137d1d1d40a021e2f436489b9d88f387edf80b
Sasa Kocic <sasa.kocic@gmail.com>
2023-05-25 08:39:13 +0200

    exported Profile

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { MyButton } from './MyButton';
 import { Greeting } from './Greeting';
 import { ProfileButton } from './ProfileButton';
 import { ShoppingList } from './ShoppingList';
+import { Profile } from './Profile';
 
 function App() {
   return (
