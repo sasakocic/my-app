acfabce3cafa8ac0e066b27bb23ca9fcdc151c0f
Sasa Kocic <sasa.kocic@gmail.com>
2023-05-25 22:04:06 +0200

    Added some styling

--- src/Imprint.js
+++ src/Imprint.js
@@ -1,31 +1,32 @@
 import React from 'react';
+import './Imprint.css';
 
 function Imprint() {
   return (
-    <div>
-      <h1>Imprint</h1>
-      <h2>IT Spektar</h2>
-      <p>Fehnweg 163, Hamburg</p>
-      <p>Phone: +49 123-456-7890</p>
-      <p>Email: info@itspektar.com</p>
-      
-      <h2>Represented by</h2>
-      <p>Sasa Kocic (CEO)</p>
-      
-      <h2>Registration Details</h2>
-      <p>Company Registration Number: 12345678</p>
-      <p>VAT ID: DE123456789</p>
-      
-      <h2>Disclaimer</h2>
-      <p>This website is for informational purposes only. We strive to keep the information on this website accurate and up-to-date, but we make no warranties or representations of any kind, express or implied, about the completeness, accuracy, reliability, suitability, or availability with respect to the website or the information contained on the website. Any reliance you place on such information is therefore strictly at your own risk.</p>
-      
-      <h2>Liability for Links</h2>
-      <p>Our website may contain links to external websites. We have no control over the content and availability of those sites and are not responsible for any damages or losses incurred through the use of those links.</p>
-      
-      <h2>Copyright</h2>
-      <p>All content on this website, including text, graphics, logos, images, and software, is the property of Company Name and is protected by international copyright laws.</p>
-      
-      <p>If you have any questions or concerns regarding this imprint, please contact us.</p>
+    <div className="Imprint-container">
+      <h1 className="Imprint-heading">Imprint</h1>
+      <h2 className="Imprint-subheading">IT Spektar</h2>
+      <p className="Imprint-content">Fehnweg 163, Hamburg</p>
+      <p className="Imprint-content">Phone: +49 123-456-7890</p>
+      <p className="Imprint-content">Email: info@itspektar.com</p>
+
+      <h2 className="Imprint-subheading">Represented by</h2>
+      <p className="Imprint-content">Sasa Kocic (CEO)</p>
+
+      <h2 className="Imprint-subheading">Registration Details</h2>
+      <p className="Imprint-content">Company Registration Number: 12345678</p>
+      <p className="Imprint-content">VAT ID: DE123456789</p>
+
+      <h2 className="Imprint-subheading">Disclaimer</h2>
+      <p className="Imprint-content">This website is for informational purposes only. We strive to keep the information on this website accurate and up-to-date, but we make no warranties or representations of any kind, express or implied, about the completeness, accuracy, reliability, suitability, or availability with respect to the website or the information contained on the website. Any reliance you place on such information is therefore strictly at your own risk.</p>
+
+      <h2 className="Imprint-subheading">Liability for Links</h2>
+      <p className="Imprint-content">Our website may contain links to external websites. We have no control over the content and availability of those sites and are not responsible for any damages or losses incurred through the use of those links.</p>
+
+      <h2 className="Imprint-subheading">Copyright</h2>
+      <p className="Imprint-content">All content on this website, including text, graphics, logos, images, and software, is the property of IT Spektar and is protected by international copyright laws.</p>
+
+      <p className="Imprint-content">If you have any questions or concerns regarding this imprint, please contact us.</p>
     </div>
   );
 }
--- src/PrivacyPolicy.js
+++ src/PrivacyPolicy.js
@@ -1,44 +1,59 @@
 import React from 'react';
+import './PrivacyPolicy.css';
 
 function PrivacyPolicy() {
   return (
-    <div>
-      <h1>Privacy Policy</h1>
+    <div className="PrivacyPolicy-container">
+      <h1 className="PrivacyPolicy-heading">Privacy Policy</h1>
       <p>Your privacy is important to us. This Privacy Policy outlines how we collect, use, and protect your personal information when you use our website.</p>
-      
-      <h2>1. Information Collection</h2>
-      <p>We may collect personal information from you when you:</p>
-      <ul>
-        <li>Sign up for an account</li>
-        <li>Submit a contact form</li>
-        <li>Make a purchase</li>
-        <li>Subscribe to our newsletter</li>
-      </ul>
-      
-      <h2>2. Information Usage</h2>
-      <p>We may use your personal information to:</p>
-      <ul>
-        <li>Provide and improve our services</li>
-        <li>Process transactions</li>
-        <li>Send periodic emails</li>
-      </ul>
-      
-      <h2>3. Information Protection</h2>
-      <p>We implement security measures to protect your personal information. However, no method of transmission over the internet or electronic storage is completely secure.</p>
-      
-      <h2>4. Information Sharing</h2>
-      <p>We do not sell, trade, or rent your personal information to third parties. We may share your information with trusted service providers who assist us in operating our website.</p>
-      
-      <h2>5. Cookies</h2>
-      <p>We use cookies to enhance your experience on our website. You can choose to disable cookies in your browser settings, but it may affect certain functionality.</p>
-      
-      <h2>6. External Links</h2>
-      <p>Our website may contain links to external sites that are not operated by us. We have no control over the content and practices of these sites, and we are not responsible for their privacy policies.</p>
-      
-      <h2>7. Updates to Privacy Policy</h2>
-      <p>We reserve the right to update this Privacy Policy at any time. We encourage you to review this page periodically for any changes.</p>
-      
-      <p>If you have any questions or concerns regarding this Privacy Policy, please contact us.</p>
+
+      <div className="PrivacyPolicy-section">
+        <h2 className="PrivacyPolicy-subheading">1. Information Collection</h2>
+        <p className="PrivacyPolicy-content">We may collect personal information from you when you:</p>
+        <ul className="PrivacyPolicy-list">
+          <li className="PrivacyPolicy-list-item">Sign up for an account</li>
+          <li className="PrivacyPolicy-list-item">Submit a contact form</li>
+          <li className="PrivacyPolicy-list-item">Make a purchase</li>
+          <li className="PrivacyPolicy-list-item">Subscribe to our newsletter</li>
+        </ul>
+      </div>
+
+      <div className="PrivacyPolicy-section">
+        <h2 className="PrivacyPolicy-subheading">2. Information Usage</h2>
+        <p className="PrivacyPolicy-content">We may use your personal information to:</p>
+        <ul className="PrivacyPolicy-list">
+          <li className="PrivacyPolicy-list-item">Provide and improve our services</li>
+          <li className="PrivacyPolicy-list-item">Process transactions</li>
+          <li className="PrivacyPolicy-list-item">Send periodic emails</li>
+        </ul>
+      </div>
+
+      <div className="PrivacyPolicy-section">
+        <h2 className="PrivacyPolicy-subheading">3. Information Protection</h2>
+        <p className="PrivacyPolicy-content">We implement security measures to protect your personal information. However, no method of transmission over the internet or electronic storage is completely secure.</p>
+      </div>
+
+      <div className="PrivacyPolicy-section">
+        <h2 className="PrivacyPolicy-subheading">4. Information Sharing</h2>
+        <p className="PrivacyPolicy-content">We do not sell, trade, or rent your personal information to third parties. We may share your information with trusted service providers who assist us in operating our website.</p>
+      </div>
+
+      <div className="PrivacyPolicy-section">
+        <h2 className="PrivacyPolicy-subheading">5. Cookies</h2>
+        <p className="PrivacyPolicy-content">We use cookies to enhance your experience on our website. You can choose to disable cookies in your browser settings, but it may affect certain functionality.</p>
+      </div>
+
+      <div className="PrivacyPolicy-section">
+        <h2 className="PrivacyPolicy-subheading">6. External Links</h2>
+        <p className="PrivacyPolicy-content">Our website may contain links to external sites that are not operated by us. We have no control over the content and practices of these sites, and we are not responsible for their privacy policies.</p>
+      </div>
+
+      <div className="PrivacyPolicy-section">
+        <h2 className="PrivacyPolicy-subheading">7. Updates to Privacy Policy</h2>
+        <p className="PrivacyPolicy-content">We reserve the right to update this Privacy Policy at any time. We encourage you to review this page periodically for any changes.</p>
+      </div>
+
+      <p className="PrivacyPolicy-content">If you have any questions or concerns regarding this Privacy Policy, please contact us.</p>
     </div>
   );
 }
